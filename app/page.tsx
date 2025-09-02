8ab991ae4b8fef93029be38ca0109c24b709ba11
kaushik06slr <kaushik06slr@gmail.com>
2025-09-02 12:13:15 +0530

    Social Link Changes

--- app/page.tsx
+++ app/page.tsx
@@ -97,10 +97,10 @@ function MagneticSocialLink({
   link: string
 }) {
   return (
-    <Magnetic springOptions={{ bounce: 0 }} intensity={0.3}>
+    <Magnetic springOptions={{ bounce: 0 }} intensity={0}>
       <a
         href={link}
-        className="group relative inline-flex shrink-0 items-center gap-[1px] rounded-full bg-zinc-100 px-2.5 py-1 text-sm text-black transition-colors duration-200 hover:bg-zinc-950 hover:text-zinc-50 dark:bg-zinc-800 dark:text-zinc-100 dark:hover:bg-zinc-700"
+        className="group relative inline-flex shrink-0 items-center gap-[1px] rounded-full bg-zinc-100 p-2.5 py-1 text-sm text-black transition-colors duration-200 dark:bg-zinc-800 dark:text-zinc-100 hover:text-zinc-100 social-link"
       >
         {children}
         <svg
